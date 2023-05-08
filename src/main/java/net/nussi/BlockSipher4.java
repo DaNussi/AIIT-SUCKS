199d2c7f476ebabd43323ab5d22bb67b714f7c4b
DaNussi <daniel.nussbaum@htlstp.at>
2023-05-08 22:16:18 +0200

    BlockSipher8 fix

--- src/main/java/net/nussi/BlockSipher4.java
+++ src/main/java/net/nussi/BlockSipher4.java
@@ -1,2 +1,28 @@
-package net.nussi;public class BlockSipher4 {
+package net.nussi;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class BlockSipher4 {
+
+    public static void main(String[] args) throws IOException {
+
+        String data = Files.readString(Path.of("data\\BlockSipher4.txt")).replaceAll("\r", "");
+
+        StringBuilder out = new StringBuilder();
+
+        for (int i = 0; i < data.length(); i+=4) {
+            out.append(data.substring(i + 0, i + 1));
+            out.append(data.substring(i + 1, i + 2));
+            out.append(data.substring(i + 3, i + 4));
+            out.append(data.substring(i + 2, i + 3));
+        }
+
+        System.out.println("Data:\n" + data);
+        System.out.println("Output:\n" + out);
+
+        Files.writeString(Path.of("data\\BlockSipher4_Output.txt"), out.toString());
+    }
+    
 }
--- src/main/java/net/nussi/BlockSipher8.java
+++ src/main/java/net/nussi/BlockSipher8.java
@@ -1,2 +1,34 @@
-package net.nussi;public class BlockSipher8 {
+package net.nussi;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class BlockSipher8 {
+
+    public static void main(String[] args) throws IOException {
+
+        String data = Files.readString(Path.of("data\\BlockSipher8.txt")).replaceAll("\r", "");
+
+        StringBuilder out = new StringBuilder();
+
+        while (data.length() % 8 != 0) { data += " "; }
+
+        for (int i = 0; i < data.length(); i+=8) {
+            out.append(data.substring(i + 0, i + 1));
+            out.append(data.substring(i + 1, i + 2));
+            out.append(data.substring(i + 2, i + 3));
+            out.append(data.substring(i + 3, i + 4));
+
+            out.append(data.substring(i + 7, i + 8));
+            out.append(data.substring(i + 6, i + 7));
+            out.append(data.substring(i + 5, i + 6));
+            out.append(data.substring(i + 4, i + 5));
+        }
+
+        System.out.println("Data:\n" + data);
+        System.out.println("Output:\n" + out);
+
+        Files.writeString(Path.of("data\\BlockSipher8_Output.txt"), out.toString());
+    }
 }
